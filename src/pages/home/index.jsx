78115f891673322f2fddf702801cec67ba3e0221
刘煜 <yua.liu@qunar.com>
2024-06-15 12:01:37 +0800

    feat: 分享

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -18,18 +18,55 @@ class Index extends Component {
 
   state = {
     loading: true,
-    productList: []
+    productList: [],
+    pid: null,
+    source: 'wx',
+    currentProduct: ''
   }
 
-  async componentDidMount () {
-    this.fetchProductList()
+  onShareAppMessage() {
+    // console.log('this.state??: ', this.state);
+    // const {currentProduct} = this.state; // 这里需要根据你的实际情况来获取 currentProduct
+    const { title = '商品', id, images } = this.currentProduct || {}
+    if (id) {
+      return {
+        title: `分享商品：${title}`, // 分享标题
+        path: `pages/home/index?pid=${id}`, // 分享链接，这里为当前页面路径
+          imgUrl: this.splitImages(images)[0],
+        success: function (res) {
+          // 用户点击分享后执行的回调函数
+          console.log('分享成功', res)
+        },
+        fail: function (err) {
+          // 分享失败的回调函数
+          console.log('分享失败', err)
+        }
+      }
+    }
+  }
+
+  onLoad (query) {
+    const pid = query.pid
+    const source = query.source
+
+    // 你也可以将这些值保存到组件的 state 或 data 中，以便在页面中使用
+    this.setState({
+      pid,
+      source
+    }, () => {
+      this.fetchProductList()
+    })
   }
 
   async fetchProductList () {
+    const { pid } = this.state;
     try {
       const { data = {} } = await Taro.request({
         url: 'http://liuyu666.cn/product/list',
         method: 'GET',
+        data: {
+          pid
+        }
       })
       this.setState({
         productList: data.data.list || {}
@@ -45,15 +82,19 @@ class Index extends Component {
     return images.split(';')
   }
 
+  handleShareButtonTap (product) {
+    this.currentProduct = product
+  }
+
   render () {
-    const { productList } = this.state
+    const { productList, pid } = this.state
     // const { counterStore: { counter } } = this.props.store
     return (
       <View className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 View-4 bg-slate-200 min-h-screen">
         {
-          productList.map(product => {
+          productList.map((product, index) => {
             return (
-              <View className="bg-white rounded-lg overflow-hidden shadow-md dark:bg-gray-800 p-1 pb-2 h-72 m-2">
+              <View className={`${(product.id == pid && index == 0) ? 'orange-shadow ': ''} bg-white rounded-lg overflow-hidden shadow-md dark:bg-gray-800 p-1 pb-2 h-72 m-2`} >
                 <Image
                   alt={product.title}
                   className="w-full h-48 object-cover"
@@ -66,7 +107,12 @@ class Index extends Component {
                   <View className="text-lg font-semibold text-gray-900 dark:text-gray-100 truncate">{product.title}</View>
                   <View className="flex items-center justify-between mt-2">
                     <View className="text-base font-semibold text-gray-900 text-red-600">¥{product.price}</View>
-                    <Button className="m-0 bg-orange-400 text-white" open-type="share" >
+                    <Button
+                      className="m-0 bg-orange-400 text-white"
+                      open-type="share"
+                      data-pid={product.id} // 在这里绑定产品的 ID
+                      onTap={() => this.handleShareButtonTap(product)} // 处理按钮的点击事件
+                    >
                       分享
                     </Button>
                   </View>
@@ -79,21 +125,6 @@ class Index extends Component {
       </View>
 
     )
-  }
-}
-
-Index.onShareAppMessage = function (options) {
-  return {
-    title: '自定义分享标题', // 分享标题
-    path: 'pages/home/index', // 分享链接，这里为当前页面路径
-    success: function (res) {
-      // 用户点击分享后执行的回调函数
-      console.log('分享成功', res)
-    },
-    fail: function (err) {
-      // 分享失败的回调函数
-      console.log('分享失败', err)
-    }
   }
 }
 
